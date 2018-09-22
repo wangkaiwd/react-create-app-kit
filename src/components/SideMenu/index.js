3b98d8eb269e1ac8e5e36a0f9e98a8b737ffdb89
wangkaiwd <1162486176@qq.com>
2018-09-22 15:05:05 +0800

    动态生成openKeys,在页面内菜单进行跳转时将调转到的页面对应的左侧导航展开

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -11,14 +11,14 @@ import leftMenuList from './config'
 import TopHeader from '../TopHeader'
 import PageContent from './content'
 
-const { Sider } = Layout
+const {Sider} = Layout
 const SubMenu = Menu.SubMenu
 
 @withRouter
 class SideMenu extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
-    let { pathname } = this.props.history.location
+    let {pathname} = this.props.history.location
     if (pathname === '/home') {
       pathname = '/home/test/example'
     }
@@ -26,38 +26,43 @@ class SideMenu extends Component {
       collapsed: false,
       theme: sessionStorage.theme || 'dark',
       defaultSelectedKeys: [pathname],
+      openKeys: []
     }
   }
+
   componentWillReceiveProps = (nextProps) => {
-    const { pathname } = nextProps.location
+    const {pathname} = nextProps.location
     if (this.props.location.pathname !== pathname) {
       this.createOpenKeys(nextProps)
-      this.setState({ defaultSelectedKeys: [pathname] })
+      this.setState({defaultSelectedKeys: [pathname]})
     }
   }
   componentDidMount = () => {
     this.createOpenKeys(this.props)
   }
+
   toggle = () => {
     this.setState({
       collapsed: !this.state.collapsed,
     })
   }
 
-  createOpenKeys = ({ location, match }) => {
+  // 生成打开的菜单项
+  createOpenKeys = ({location, match}) => {
     const paths = location.pathname.split('/')
     const newPaths = paths.slice(2, paths.length - 1)
     let str = match.path
     const openKeys = newPaths.map(item => str += `/${item}`)
-    this.setState({ openKeys })
+    this.setState({openKeys})
   }
+  // 生成子菜单
   createSubMenus = (leftMenuList) => (
     leftMenuList.map(item => {
       if (!item.options && !item.children) {
         return (
           <Menu.Item key={item.key}>
             <Link to={item.key} key={item.key}>
-              {item.icon && <Icon type={item.icon} />}
+              {item.icon && <Icon type={item.icon}/>}
               <span>{item.text}</span>
             </Link>
           </Menu.Item>
@@ -68,7 +73,7 @@ class SideMenu extends Component {
           key={item.key}
           title={
             <span>
-              {item.icon && <Icon type={item.icon} />}
+              {item.icon && <Icon type={item.icon}/>}
               <span>{item.text}</span>
             </span>
           }>
@@ -77,12 +82,12 @@ class SideMenu extends Component {
         </SubMenu>)
     })
   )
-
+  // 生成列表菜单
   createMenus = (menus) => (
     menus.map(sub => (
       <Menu.Item key={sub.key}>
         <Link to={sub.key}>
-          {sub.icon && <Icon type={sub.icon} />}
+          {sub.icon && <Icon type={sub.icon}/>}
           <span>{sub.text}</span>
         </Link>
       </Menu.Item>
@@ -90,22 +95,23 @@ class SideMenu extends Component {
   )
   changeTheme = (value) => {
     this.setState(
-      { theme: value ? 'dark' : 'light' },
+      {theme: value ? 'dark' : 'light'},
       () => sessionStorage.setItem('theme', this.state.theme)
     )
   }
 
-  onSelect = ({ selectedKeys }) => {
-    console.log('select');
-    let { defaultSelectedKeys } = this.state
-    defaultSelectedKeys = selectedKeys
-    this.setState({ defaultSelectedKeys })
+  onSelect = ({selectedKeys}) => {
+    this.setState({defaultSelectedKeys: selectedKeys})
   }
 
-  render() {
-    const siderStyle = { backgroundColor: '#fff' }
-    const { collapsed, theme, defaultSelectedKeys, openKeys } = this.state
-    console.log(openKeys);
+  // submenu展开关闭的回调
+  onOpenChange = (openKeys) => {
+    this.setState({openKeys})
+  }
+
+  render () {
+    const siderStyle = {backgroundColor: '#fff'}
+    const {collapsed, theme, defaultSelectedKeys, openKeys} = this.state
     return (
       <Layout className="side-menu-component">
         <Sider
@@ -129,13 +135,14 @@ class SideMenu extends Component {
             onSelect={this.onSelect}
             selectedKeys={defaultSelectedKeys}
             openKeys={openKeys}
+            onOpenChange={this.onOpenChange}
           >
             {this.createSubMenus(leftMenuList)}
           </Menu>
         </Sider>
         <Layout>
-          <TopHeader collapsed={collapsed} toggle={this.toggle} />
-          <PageContent />
+          <TopHeader collapsed={collapsed} toggle={this.toggle}/>
+          <PageContent/>
         </Layout>
       </Layout>
     )
